3e4fc8e7f29eceb38e2ea1863c87abdd4f245fc7
martin_margonari <martinmargonari@gmail.com>
2015-10-13 15:30:52 -0300

    Agregada Activity de Log In

---
+++ FDrive/app/src/main/java/com/example/margonari/fdrive/LogInActivity.java
@@ -0,0 +1,49 @@
+package com.example.margonari.fdrive;
+
+import android.annotation.TargetApi;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LogInActivity extends AppCompatActivity {
+
+    private EditText textEmail, textPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+
+        textEmail = (EditText) findViewById(R.id.editTextEmail);
+        textEmail.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    textEmail.setBackgroundTintList(ColorStateList.valueOf(Color.BLUE));
+                } else {
+                    textEmail.setBackgroundTintList(ColorStateList.valueOf(Color.GRAY));
+                }
+            }
+        });
+
+        textPassword = (EditText) findViewById(R.id.editTextPassword);
+        textPassword.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    textPassword.setBackgroundTintList(ColorStateList.valueOf(Color.BLUE));
+                } else {
+                    textPassword.setBackgroundTintList(ColorStateList.valueOf(Color.GRAY));
+                }
+            }
+        });
+
+    }
+}
